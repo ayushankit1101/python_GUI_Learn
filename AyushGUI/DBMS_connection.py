6c3b4f5e9258b772108794267ac272ecd7d327e7
ayushankit1101 <realayushankit@gmail.com>
2025-07-29 12:51:30 +0530

    button fn updated

--- AyushGUI/DBMS_connection.py
+++ AyushGUI/DBMS_connection.py
@@ -1,7 +1,7 @@
 import mysql.connector
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QApplication, QHBoxLayout, QLineEdit, QGridLayout, QComboBox, \
-    QTextEdit, QRadioButton, QPushButton, QMessageBox
 from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QComboBox, QGridLayout, QLabel, QLineEdit, QTextEdit, QHBoxLayout, \
+    QRadioButton, QPushButton, QApplication, QMessageBox
 
 
 class SignupWindow(QWidget):
@@ -36,33 +36,8 @@ class SignupWindow(QWidget):
                         background-color: #005bb5;
                 }
         """)
-        # self.login_page()
+
         self.setup_ui()
-
-
-
-    def login_page(self):
-        vbox = QVBoxLayout()
-        self.setLayout(vbox)
-
-        self.username = QLineEdit()
-        self.username.setPlaceholderText("USERNAME")
-
-        self.password = QLineEdit()
-        self.password.setPlaceholderText("PASSWORD")
-
-        vbox.addWidget(self.username)
-        vbox.addWidget(self.password)
-
-        btn_layout = QHBoxLayout()
-        self.login_btn = QPushButton("Login")
-        self.signup_btn = QPushButton("Signup")
-        btn_layout.addWidget(self.login_btn)
-        btn_layout.addWidget(self.signup_btn)
-        vbox.addLayout(btn_layout)
-
-        self.login_btn.clicked.connect(self.check_login)
-        self.signup_btn.clicked.connect(self.setup_ui)
 
 
 
@@ -75,7 +50,7 @@ class SignupWindow(QWidget):
         self.course_combo.setPlaceholderText("CHOOSE A COURSE")
         self.course_combo.addItems(["PYTHON", "MACHINE LEARNING", "DATA SCIENCE", "FULL STACK"])
 
-        self.course_combo.currentTextChanged.connect(self.update_fee)
+        # self.course_combo.currentTextChanged.connect(self.update_fee)
 
         form_layout = QGridLayout()
 
@@ -119,7 +94,7 @@ class SignupWindow(QWidget):
         radio_layout.addWidget(self.yes_radio)
         radio_layout.addWidget(self.no_radio)
         form_layout.addLayout(radio_layout, 8, 1)
-        self.yes_radio.toggled.connect(self.toggle_installment_field)
+        # self.yes_radio.toggled.connect(self.toggle_installment_field)
 
         form_layout.addWidget(QLabel("INSTALLMENT AMOUNT"), 9, 0)
         self.installment_display = QLineEdit()
@@ -137,43 +112,14 @@ class SignupWindow(QWidget):
         btn_layout.addWidget(self.login_btn_page, alignment=Qt.AlignmentFlag.AlignCenter)
         btn_layout.addWidget(self.signup_btn, alignment=Qt.AlignmentFlag.AlignCenter)
         vbox.addLayout(btn_layout)
+
         self.signup_btn.clicked.connect(self.insert_data)
         self.login_btn_page.clicked.connect(self.show_login)
 
-    def show_signup(self):
-        self.hide()
-        self.setup_ui.show()
-
     def show_login(self):
+        self.login_page = LoginWindow()
+        self.login_page.show()
         self.hide()
-        self.show()
-
-
-
-
-
-    def check_login(self):
-        username = self.username.text().strip()
-        password = self.password.text().strip()
-        try:
-            mydb = mysql.connector.connect(
-                host="localhost",
-                user="root",
-                password="7266",
-                database="login_info"
-            )
-            mycur = mydb.cursor()
-            query = 'SELECT password FROM user_data WHERE username = "username" and password = "password"'
-            mycur.execute(query, (username,password))
-            data = mycur.fetchall()
-            mydb.close()
-            if data:
-                self.open_main_window()
-            else:
-                QMessageBox.warning(self, "Error", "invalid Username and Password")
-
-        except mysql.connector.Error as err:
-            QMessageBox.critical(self, "Database Error", str(err))
 
     def update_fee(self, selected_course):
         fee_dict = {
@@ -225,6 +171,92 @@ class SignupWindow(QWidget):
             print("Unexpected error:", e)
 
 
+class LoginWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("SIGNUP WINDOW")
+        self.setGeometry(100, 100, 500, 500)
+        self.setStyleSheet("""
+                    QWidget {
+                            background-color: #f4f6f8;
+                            font-family: 'Segoe UI';
+                            font-size: 14px;
+                    }
+                    QLabel {
+                            font-weight: 600;
+                            font-family: "Segoe UI";
+                            color: #333;
+                    }
+                    QLineEdit, QTextEdit, QComboBox {
+                            border: 1px solid #ccc;
+                            border-radius: 6px;
+                            padding: 6px;
+                            background: #fff;
+                    }
+                    QPushButton {
+                            background-color: #0066cc;
+                            color: white;
+                            padding: 8px 16px;
+                            border-radius: 6px;
+                    }
+                    QPushButton:hover {
+                            background-color: #005bb5;
+                    }
+            """)
+        self.login_page()
+
+    def login_page(self):
+        vbox = QVBoxLayout()
+        self.setLayout(vbox)
+
+        self.username = QLineEdit()
+        self.username.setPlaceholderText("USERNAME")
+
+        self.password = QLineEdit()
+        self.password.setPlaceholderText("PASSWORD")
+
+        vbox.addWidget(self.username)
+        vbox.addWidget(self.password)
+
+        btn_layout = QHBoxLayout()
+        self.login_btn = QPushButton("Login")
+        self.signup_btn = QPushButton("Signup")
+        btn_layout.addWidget(self.login_btn)
+        btn_layout.addWidget(self.signup_btn)
+        vbox.addLayout(btn_layout)
+
+        self.signup_btn.clicked.connect(self.show_signup_page)
+
+    def show_signup_page(self):
+        self.signup_page = SignupWindow()
+        self.signup_page.show()
+        self.hide()
+
+    def check_login(self):
+        username = self.username.text().strip()
+        password = self.password.text().strip()
+        try:
+            mydb = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="7266",
+                database="login_info"
+            )
+            mycur = mydb.cursor()
+            query = 'SELECT password FROM user_data WHERE username = "username" and password = "password"'
+            mycur.execute(query, (username,password))
+            data = mycur.fetchall()
+            mydb.close()
+            if data:
+                self.open_main_window()
+            else:
+                QMessageBox.warning(self, "Error", "invalid Username and Password")
+
+        except mysql.connector.Error as err:
+            QMessageBox.critical(self, "Database Error", str(err))
+
+# class BlankWindow(QWidget):
+#     def __init__(self):
 
 
 
